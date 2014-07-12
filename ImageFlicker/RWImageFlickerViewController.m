33fc2eb3ccec133f98318a5febeb9fc13d81fcf2
Robert Wagstaff <robertwagstaff@Roberts-MacBook-Pro-2.local>
2014-07-12 11:52:11 -0700

    added dummy collection view cells.

--- ImageFlicker/RWImageFlickerViewController.m
+++ ImageFlicker/RWImageFlickerViewController.m
@@ -8,6 +8,11 @@
 
 #import "RWImageFlickerViewController.h"
 
+#define REUSE_IDENTIFIER @"imageCellIdentifier"
+#define CELL_MARGIN (SCREEN_WIDTH * 0.025)
+#define CELL_WIDTH (SCREEN_WIDTH * 0.3)
+#define CELL_HEIGHT (SCREEN_WIDTH * 0.3)
+
 @interface RWImageFlickerViewController ()
 
 @property (nonatomic, strong) UICollectionView *collectionView;
@@ -16,6 +21,7 @@
 
 @implementation RWImageFlickerViewController
 
+#pragma mark - view lifecycle
 - (id)init
 {
     self = [super init];
@@ -28,38 +34,59 @@
 - (void)viewDidLoad
 {
     self.view = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self setupCollectionView];
+    [super viewDidLoad];
+}
+
+
+#pragma mark - setup
+-(void) setupCollectionView
+{
+    UICollectionViewFlowLayout* layout = [UICollectionViewFlowLayout new];
+//    layout.minimumLineSpacing = 50;
+//    layout.minimumInteritemSpacing = 0;
+//    [self.flowLayout setItemSize:CGSizeMake(191, 160)];
+    [layout setScrollDirection:UICollectionViewScrollDirectionVertical];
+    layout.minimumInteritemSpacing = 0;
+    layout.sectionInset = UIEdgeInsetsMake(CELL_MARGIN,CELL_MARGIN,CELL_MARGIN,CELL_MARGIN);
     
-    UICollectionViewFlowLayout *layout=[[UICollectionViewFlowLayout alloc] init];
-    _collectionView=[[UICollectionView alloc] initWithFrame:self.view.frame collectionViewLayout:layout];
-    [_collectionView setDataSource:self];
-    [_collectionView setDelegate:self];
     
-    [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellIdentifier"];
-    [_collectionView setBackgroundColor:[UIColor redColor]];
+    CGRect collectionViewFrame = CGRectMake(self.view.frame.origin.x, self.view.frame.origin.y + 100, self.view.frame.size.width, self.view.frame.size.height - 100);
+    self.collectionView=[[UICollectionView alloc] initWithFrame:collectionViewFrame collectionViewLayout:layout];
+    [self.collectionView setDataSource:self];
+    [self.collectionView setDelegate:self];
+    
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:REUSE_IDENTIFIER];
+    [self.collectionView setBackgroundColor:[UIColor redColor]];
+    self.collectionView.bounces = YES;
+    [self.collectionView setShowsHorizontalScrollIndicator:NO];
+    [self.collectionView setShowsVerticalScrollIndicator:NO];
     
     [self.view addSubview:_collectionView];
-    
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
+#pragma mark - UICollectionViewDataSource
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return 15;
+    return 24;
 }
 
-// The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    UICollectionViewCell *cell=[collectionView dequeueReusableCellWithReuseIdentifier:@"cellIdentifier" forIndexPath:indexPath];
-    
-    cell.backgroundColor=[UIColor greenColor];
+    UICollectionViewCell *cell=[collectionView dequeueReusableCellWithReuseIdentifier:REUSE_IDENTIFIER forIndexPath:indexPath];
+    if (indexPath.row % 2 == 0) {
+        cell.backgroundColor=[UIColor greenColor];
+    } else {
+        cell.backgroundColor=[UIColor blueColor];
+    }
     return cell;
 }
 
+#pragma mark - UICollectionViewDelegateFlowLayout
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath
 {
-    return CGSizeMake(50, 50);
+    return CGSizeMake(CELL_WIDTH, CELL_HEIGHT);
 }
+
 
 @end
